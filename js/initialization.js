79699317ac767a10cc56219ec29cfc234223043f
Guilherme França da Silva <francaguilherme27@gmail.com>
2026-03-12 18:23:27 -0300

    refactor: optimization and correction

--- js/initialization.js
+++ js/initialization.js
@@ -1,4 +1,5 @@
 const bootDisplay = document.getElementById("boot");
+const terminal = document.getElementById("terminal");
 const skip = document.getElementById("skip");
 
 const bootLines = [
@@ -24,35 +25,45 @@ const bootLines = [
 "GX Desktop Ready.",
 "",
 "",
--1
+{ type: "end" }
 ];
 
 function startSystem () {
-    const terminal = document.getElementById("terminal");
+    terminal.classList.add("exit");
 
-    if (terminal instanceof HTMLElement) {
-        terminal.classList.add("exit");
+    setTimeout(() => {
+        terminal.style.display = "none"
+    }, 500);
 
-        setTimeout(() => {
-            terminal.style.display = "none"
-        }, 500);
-    }
-
-    document.removeEventListener("keydown");
-    document.removeEventListener("click");
+    document.removeEventListener("keydown", startSystem);
+    document.removeEventListener("click", startSystem);
 }
 
 function delay(ms){
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-// 
+function lineRandomDelay() {
+    const min = 50;
+    const max = 150;
+
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+function addExitListeners() {
+    document.addEventListener("keydown", startSystem);
+    document.addEventListener("touchend", startSystem);
+}
+
+function changeVisibility() {
+    skip.style.display = "none";
+
+    bootDisplay.classList.remove("loading-started");
+    bootDisplay.classList.add("loading-complete");
+}
+
 async function typeBoot(){
-
     for (const text of bootLines){
-        const min = 50;
-        const max = 100;
-
         const line = document.createElement("p");
         bootDisplay.appendChild(line);
 
@@ -60,29 +71,24 @@ async function typeBoot(){
             line.innerHTML = "&nbsp;";
         }
 
-        if (text === -1) {
-            skip.style.display = "none";
-
+        if (text.type === "end") {
             line.classList.add("blink");
             line.innerHTML = "< PRESS ANY KEY TO START >";
-            
-            bootDisplay.classList.remove("loading-started");
-            bootDisplay.classList.add("loading-complete");
 
-            document.addEventListener("keydown", () => startSystem());
-            document.addEventListener("click", () => startSystem());
+            changeVisibility();
+            addExitListeners();
 
             continue;
         }
 
         for (const letter of text){
-            line.innerHTML += letter;
-            await delay();
+            line.textContent += letter;
+            await delay(1);
         }
 
         bootDisplay.scrollTop = bootDisplay.scrollHeight;
 
-        await delay(Math.floor(Math.random() * (max - min + 1)) + min);
+        await delay(lineRandomDelay());
     }
 }
 
