325171af6d287dcc81d974fe500ce8ff04364b7f
Guilherme França da Silva <francaguilherme27@gmail.com>
2026-04-27 11:01:08 -0300

    wip: dialog interface

---
+++ components/dialog/dialog.js
@@ -0,0 +1,35 @@
+let dialogTemplateCache = null;
+let desktop = document.getElementById("main");
+
+async function loadDialogTemplate() {
+  if (dialogTemplateCache) return dialogTemplateCache;
+
+  const res = await fetch("./components/dialog/dialog.html");
+  const html = await res.text();
+
+  dialogTemplateCache = html;
+  return html;
+}
+
+async function openDialog(config = {}) {
+  const {
+    title = "Aviso",
+    message = "",
+  } = config;
+
+  const html = await loadDialogTemplate();
+
+  const wrapper = document.createElement("div");
+  wrapper.innerHTML = html;
+
+  const dialog = wrapper.firstElementChild;
+
+  dialog.querySelector(".dialog-title").textContent = title;
+  dialog.querySelector(".dialog-message").textContent = message;
+
+  const close = () => dialog.remove();
+  dialog.querySelector(".dialog-close").onclick = close;
+  dialog.querySelector(".dialog-confirm").onclick = close;
+
+  desktop.appendChild(dialog);
+}
