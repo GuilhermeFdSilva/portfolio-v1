94cb2f7c4f944096d1e8f914551f9884cc7a2661
Guilherme França da Silva <francaguilherme27@gmail.com>
2026-05-26 15:06:07 -0300

    feat: add session screen initialization to main.js

--- js/main.js
+++ js/main.js
@@ -4,13 +4,20 @@ import { Taskbar } from "../components/taskbar/taskbar.js";
 
 import { Dialog } from "../components/dialog/dialog.js";
 
+import { SessionScreem } from "../components/sessionScreem/sessionScreem.js";
+
 const dialog = new Dialog(document.getElementById("desktop"));
 
 const sysBoot = new SysBoot(document.getElementById("boot-container"));
 
 sysBoot.startBoot();
 
+const sessionScreem = await SessionScreem.getSessionScreem();
+await document.body.appendChild(sessionScreem);
+
 if (true) {
+    
+
     const taskbar = await Taskbar.getTaskbar();
     document.getElementById("taskbar").appendChild(taskbar);
 
